96bcbf262c79006bebb1a0517cc5082fee3fd1a2
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-06-07 07:49:15 -0500

    test_signed_off_by: include unittest for correct formatting
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/patch/signed_off_by/test_signed_off_by.sh
+++ tests/patch/signed_off_by/test_signed_off_by.sh
@@ -7,11 +7,33 @@ TESTMBOX=$3
 # source the runner's lib, containing function definitions
 source $PTSUITE/selftest/librunner.sh
 
-# test pass
-exec_patchtest $TESTMBOX $PASS
+test_signed_off_presence() {
+    # test pass
+    exec_patchtest $TESTMBOX $PASS
 
-# test fail
-TMP=$(mktemp)
-sed -e 's/^+Signed-off-by:.*/+/g' $TESTMBOX > $TMP
-exec_patchtest $TMP $FAIL
-rm $TMP
+    # test fail
+    TMP=$(mktemp)
+    sed -e 's/^+Signed-off-by:.*/+/g' $TESTMBOX > $TMP
+    exec_patchtest $TMP $FAIL
+    rm $TMP
+}
+
+test_signed_off_by_format() {
+    # test pass
+    exec_patchtest $TESTMBOX $PASS
+
+    # test fail
+    TMP=$(mktemp)
+    sed -e 's/^+Signed-off-by:.*/+Signed-off-by:just name/g' $TESTMBOX > $TMP
+    exec_patchtest $TMP $FAIL
+    rm $TMP
+
+    # test fail
+    TMP=$(mktemp)
+    sed -e 's/^+Signed-off-by:.*/+Signed-off-by:<just@email.com>/g' $TESTMBOX > $TMP
+    exec_patchtest $TMP $FAIL
+    rm $TMP
+}
+
+test_signed_off_presence
+test_signed_off_by_format
