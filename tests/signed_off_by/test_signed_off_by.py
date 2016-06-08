d58b773f0a7b75168c0d16703a9125537b5530bd
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-06-08 06:54:07 -0500

    signed_off_by/test_signed_off_by.py: fix test case
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/signed_off_by/test_signed_off_by.py
+++ tests/signed_off_by/test_signed_off_by.py
@@ -3,7 +3,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from oembox import OEMBox
 from parse_signed_off_by import signed_off_by, signed_off_by_mark
 from pyparsing import ParseException
-from re import match
+from re import match, search
 
 class OESignedOffBy(OEMBox):
 
@@ -14,7 +14,8 @@ class OESignedOffBy(OEMBox):
         """Test presence of 'Signed-off-by'"""
         for mbox in OESignedOffBy.mboxes:
             for message in mbox:
-                self.assertRegexpMatches(message.get_payload(), self.mark,
+                # match self.mark with no '+' preceding it
+                self.assertRegexpMatches(message.get_payload(), "(?<!\+)%s" % self.mark,
                                          "Patch must be signed-off, please git configure your name/email and commit your changes with git commit --signoff")
 
     def test_signed_off_by_format(self):
@@ -22,8 +23,8 @@ class OESignedOffBy(OEMBox):
         for mbox in OESignedOffBy.mboxes:
             for message in mbox:
                 payload = message.get_payload()
-                if not payload:
-                    self.skipTest('Empty payload, no reason to execute signed-off-by format tests')
+                if not payload or not search("(?<!\+)%s" % self.mark, payload):
+                    self.skipTest("%s not present, skipping format test" % self.mark)
                 for line in payload.splitlines():
                     if match(self.mark, line):
                         try:
