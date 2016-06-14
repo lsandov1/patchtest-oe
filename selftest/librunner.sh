aecdaf8aabceefba26c0108f452412e7f302d00f
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-06-14 07:45:27 -0500

    librunner: include optional branch parameter when executing patchtest
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- selftest/librunner.sh
+++ selftest/librunner.sh
@@ -10,9 +10,14 @@ exec_patchtest() {
     local TESTID=$2
     local TESTMBOX=$3
     local EXPECTED=$4
+    local BRANCH=$5
+
+    if [ -z "$BRANCH" ]; then
+	BRANCH="master"
+    fi
 
     # in case of failure, this RESULT will be blank
-    RESULT=$(patchtest --test-dir $TESTDIR -m $TESTMBOX --no-patch | \
+    RESULT=$(patchtest --test-dir $TESTDIR -m $TESTMBOX --no-patch --branch $BRANCH | \
 	sed -n -e "/$TESTID/p" | \
 	sed -n -e "/$EXPECTED/p")
     [ -z "${RESULT}" ] && { echo "$TESTDIR $TESTID $EXPECTED"; }
