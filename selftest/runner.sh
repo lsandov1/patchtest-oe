9f881cdd68cef844cd2968366838bf0813e55956
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-06-14 07:46:47 -0500

    runner.sh: include optional test case directory as runner parameter (defaults to tests)
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- selftest/runner.sh
+++ selftest/runner.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 TESTMBOX=$1
+TESTCASEDIR=$2
 
 if [ -z "$TESTMBOX" ]; then
     echo "Please provide a filepath to a mbox file"
@@ -9,17 +10,18 @@ else
     TESTMBOX=$(realpath $TESTMBOX)
 fi
 
-# local variables
 CD=$(dirname $0)
 SELFTESTDIR=$(realpath $CD)
-
-# variables exported to test scripts + TEST
 PTSUITE=$(dirname $SELFTESTDIR)
+TESTSDIR="$PTSUITE/tests"
 
 source $SELFTESTDIR/librunner.sh
 
-for TESTPATH in $(find $PTSUITE -name '*.sh' | \
-                sed -n -e "/selftest/!p"); do
+if [ -z $TESTCASEDIR ]; then
+    TESTCASEDIR=$TESTSDIR
+fi
+
+for TESTPATH in $(find $TESTCASEDIR -name '*.sh' | sed -e "/selftest/d"); do
 
     # test case directory
     TESTDIR=$(dirname $TESTPATH)
