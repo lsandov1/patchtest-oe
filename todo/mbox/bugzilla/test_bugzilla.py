1c0eb5269e0843013d485de3e316743153928657
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-05 03:03:46 -0500

    test_bugzilla: update test to latest messages
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/mbox/bugzilla/test_bugzilla.py
+++ todo/mbox/bugzilla/test_bugzilla.py
@@ -1,14 +1,20 @@
 import sys, os
+from re import compile
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from oebase import OEBase
-import mboxmsg as msg
+from oebase import OEBase, info
+
 
 class OEBugzilla(OEBase):
+    base_url = "https://bugzilla.yoctoproject.org/show_bug.cgi?id=%s"
+
     def test_bugzilla_entry_format(self):
+        rexp_detect = compile("\[.*[Y|y][O|o][C|c][T|t][O|o].*\]")
+        rexp_validation = compile("^\[YOCTO #(\d+)\]$")
         for message in OEBugzilla.mbox:
             payload = message.get_payload()
-            raise self.fail(self.formaterror(msg.test_bugzilla_entry_format.reason,
-                                             msg.test_bugzilla_entry_format.error,
-                                             msg.test_bugzilla_entry_format.fix,
-                                             status='TODO'))
+            for line in payload.splitlines():
+                if rexp_detect.match(line):
+                    if not rexp_validation.match(line):
+                        self.fail([('Entry', line)])
 
