1ad7e6e20174f07867499919acc4cb2fabe35f49
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-07 07:19:28 -0500

    todo/patch/cve/test_cve: templates and skip tests
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/patch/cve/test_cve.py
+++ todo/patch/cve/test_cve.py
@@ -1,16 +1,13 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from oebase import OEBase
-import patchmsg as msg
+from unittest import skip
 
+@skip('Not implemented yet')
 class OECVE(OEBase):
     def test_cve_presence_on_subject(self):
         for patch in OECVE.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_cve_presence_on_subject.reason,
-                                       msg.test_cve_presence_on_subject.error,
-                                       msg.test_cve_presence_on_subject.fix,
-                                       status='TODO'))
 
     def test_cve_tag_format(self):
         # tip: make a pyparsing file specifying syntax and parse
@@ -19,8 +16,4 @@ class OECVE(OEBase):
         #    https://bugzilla.yoctoproject.org/show_bug.cgi?id=9249
         for patch in OECVE.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_cve_tag_format.reason,
-                                       msg.test_cve_tag_format.error,
-                                       msg.test_cve_tag_format.fix,
-                                       status='TODO'))
 
