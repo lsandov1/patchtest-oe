c1714d625e18235097f9d37984f721d0f592c00b
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-07 07:12:47 -0500

    todo/metadata/src_uri/test_src_uri: template for the planning tests
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/metadata/src_uri/test_src_uri.py
+++ todo/metadata/src_uri/test_src_uri.py
@@ -1,23 +1,14 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from oebase import OEBase
-import metadatamsg as msg
+from unittest import skip
 
+@skip('Not implemented yet')
 class OESrcUri(OEBase):
-
     def test_src_uri_left_files(self):
         for patch in OESrcUri.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_src_uri_left_files.reason,
-                                       msg.test_src_uri_left_files.error,
-                                       msg.test_src_uri_left_files.fix,
-                                       status='TODO'))
 
     def test_src_uri_checksums_not_changed(self):
         for patch in OESrcUri.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_src_uri_checksums_not_changed.reason,
-                                       msg.test_src_uri_checksums_not_changed.error,
-                                       msg.test_src_uri_checksums_not_changed.fix,
-                                       status='TODO'))
-
