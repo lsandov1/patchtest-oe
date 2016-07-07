a1cfb00407d56da575720113606df8573ac5744b
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-07 07:10:08 -0500

    todo/metadata/lic_files_chksum: skip tests and update fail messages
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/metadata/lic_files_chksum/test_lic_files_chksum.py
+++ todo/metadata/lic_files_chksum/test_lic_files_chksum.py
@@ -1,24 +1,14 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-
 from oebase import OEBase
-import metadatamsg as msg
+from unittest import skip
 
+@skip('Not implemented yet')
 class OELicFilesChksum(OEBase):
-
     def test_lic_files_chksum_presence(self):
         for patch in OELicFilesChksum.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_lic_files_chksum_presence.reason,
-                                       msg.test_lic_files_chksum_presence.error,
-                                       msg.test_lic_files_chksum_presence.fix,
-                                       status='TODO'))
 
     def test_lic_files_chksum_modified_not_mentioned(self):
         for patch in OELicFilesChksum.patchset:
             payload = str(patch)
-            self.fail(self.formaterror(msg.test_lic_files_chksum_modified_not_mentioned.reason,
-                                       msg.test_lic_files_chksum_modified_not_mentioned.error,
-                                       msg.test_lic_files_chksum_modified_not_mentioned.fix,
-                                       status='TODO'))
-
