0ce710cf7feee2959daa4950a78662c08cc053e1
Jose Lamego <jose.a.lamego@linux.intel.com>
2016-07-10 18:41:58 -0500

    metadata.lic_files_chksum: Test for LIC_FILES_CHECKSUM presence/modification
    Any patch with a new .bb or .bbappend file must contain the required
    license file information.
    Any patch that modifies a .bb or .bbappend file and that includes
    modifications to the license file information must include the
    LIC_LICENSE_CHKSUM string in the patch's subject.
    
    Signed-off-by: Jose Lamego <jose.a.lamego@linux.intel.com>

--- todo/metadata/lic_files_chksum/test_lic_files_chksum.py
+++ todo/metadata/lic_files_chksum/test_lic_files_chksum.py
@@ -2,13 +2,54 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from oebase import OEBase
 from unittest import skip
+from parse_subject import subject
 
-@skip('Not implemented yet')
 class OELicFilesChksum(OEBase):
     def test_lic_files_chksum_presence(self):
         for patch in OELicFilesChksum.patchset:
-            payload = str(patch)
+            # Get relevant patches: new .bb and .bbappend
+            if patch.path.endswith('.bb') or patch.path.endswith('.bbappend'):
+                # If this patch adds the .bb or .bbappend file
+                if patch.is_added_file:
+                    payload = str(patch)
+                    # verify that patch includes license file information
+                    if ((not payload.find('LIC_FILES_CHKSUM =') == -1) |
+                        (not payload.find('LIC_FILES_CHKSUM ?=') == -1)):
+                        # if it does, then we are ok
+                        return
+                    else:
+                        # if it does not include it, patch does not have 
+                        # required license file information
+                        raise self.fail()
+
 
     def test_lic_files_chksum_modified_not_mentioned(self):
+        self.sub = 'subject'
         for patch in OELicFilesChksum.patchset:
-            payload = str(patch)
+            # Get relevant patches: modified .bb and .bbappend
+            if patch.path.endswith('.bb') or patch.path.endswith('.bbappend'):
+                if patch.is_modified_file:
+                    payload = str(patch)
+                    # verify if license file information is modified
+                    if ((not payload.find('-LIC_FILES_CHKSUM ') == -1) |
+                        (not payload.find('+LIC_FILES_CHKSUM ') == -1)):
+                        # verify that the modification is mentioned in subject
+                        for message in OELicFilesChksum.mbox:
+                            # Fail if subject is empty
+                            try:
+                                msgsubj = message[self.sub]
+                            except:
+                                self.fail()
+                            # If subject contains the mention, we are ok
+                            if (not msgsubj.find('LIC_FILES_CHKSUM') == -1):
+                                return
+                            # Split commit message from diff
+                            fullmsg = message.get_payload(decode=True)
+                            msglimit = message.get_payload().find('---')
+                            commsg = fullmsg[:msglimit]
+
+                            # If the mention is in commit message, we're ok
+                            if (not commsg.find('LIC_FILES_CHKSUM') == -1):
+                                return
+                            else:
+                                self.fail()
