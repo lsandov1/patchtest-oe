a74d349c0de05cd2ab932a8501509486c5e50f4d
Mariano Lopez <mariano.lopez@linux.intel.com>
2016-07-12 10:15:41 -0500

    todo/patch/cve: Add CVE checks
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/patch/cve/test_cve.py
+++ todo/patch/cve/test_cve.py
@@ -1,19 +1,27 @@
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from oebase import OEBase
-from unittest import skip
 
-@skip('Not implemented yet')
 class OECVE(OEBase):
+
+    re_cve_pattern = re.compile("CVE\-\d{4}\-\d+", re.IGNORECASE)
+    re_cve_word    = re.compile("\s*\+CVE\s?", re.IGNORECASE)
+    re_cve_tag     = re.compile("CVE:(\s+CVE\-\d{4}\-\d+)+", re.IGNORECASE)
+
     def test_cve_presence_on_subject(self):
-        for patch in OECVE.patchset:
-            payload = str(patch)
+        """
+        Checks if CVE-xxxx-xxxx is in subject if CVE word is in the payload.
+        """
+        for i in xrange(OECVE.nmessages):
+            if self.re_cve_word.search(OECVE.payloads[i]):
+                if not self.re_cve_pattern.search(OECVE.subjects[i]):
+                    self.fail()
 
     def test_cve_tag_format(self):
-        # tip: make a pyparsing file specifying syntax and parse
-        # string on the the test check mbox/subject test case
-        # Request from:
-        #    https://bugzilla.yoctoproject.org/show_bug.cgi?id=9249
-        for patch in OECVE.patchset:
-            payload = str(patch)
-
+        """
+        Checks if patch contains CVE tag if "CVE-xxxx-xxxx" is in subject.
+        """
+        for i in xrange(OECVE.nmessages):
+            if self.re_cve_pattern.search(OECVE.subjects[i]):
+                if not self.re_cve_tag.search(OECVE.payloads[i]):
+                    self.fail()
