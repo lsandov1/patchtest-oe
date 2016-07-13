bb507ab5feab6cf1820a5621eccca56b7ddcb439
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-13 02:24:57 -0500

    test_descrition: use class descriptions array for this check
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/mbox/description/test_description.py
+++ todo/mbox/description/test_description.py
@@ -5,29 +5,12 @@ from parse_signed_off_by import signed_off_by_mark
 
 class OEDescription(OEBase):
 
-    @classmethod
-    def setUpClassLocal(cls):
-        cls.mark = str(signed_off_by_mark).strip('"')
-        cls.final_description_marks = [cls.mark, '---']
-
-    def final_description_reached(self, line):
-        for mark in OEDescription.final_description_marks:
-            if line.startswith(mark):
-                return True
-        return False
-
     def test_description_presence(self):
-        for message in OEDescription.mbox:
-            payload = message.get_payload()
-
-            # find the position where the description ends
-            pos = min([payload.find(mark) for mark in OEDescription.final_description_marks])
-
+        for description in OEDescription.descriptions:
             # in case there are no description at all
-            if not pos:
+            if not description:
                 self.fail()
-
             # in case the description are just white spaces
-            if not payload[0:pos].strip():
+            if not description.strip():
                 self.fail()
 
