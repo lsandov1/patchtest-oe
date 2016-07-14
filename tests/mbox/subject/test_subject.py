af6b647f2ff40e3a5cd2ddc5872f94cfce2810c8
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-14 07:33:34 -0500

    test_subject: use Base.subjects variable instead
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/mbox/subject/test_subject.py
+++ tests/mbox/subject/test_subject.py
@@ -1,37 +1,22 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 
-from base import Base, warn, info
+from base import Base
 import parse_subject
 from pyparsing import ParseException
 
 class Subject(Base):
 
-    maxlength = 50
-
-    def setUp(self):
-        self.key      = 'subject'
-        self.subjects = list()
-        endprefix = ']'
-
-        # go through all subjects and lstrip the first '[]' and replace newlines
-        for message in Subject.mbox:
-            subject         = message[self.key]
-            closebracketpos = subject.find(endprefix)
-            if closebracketpos < 0:
-                warn('Patch does not contain square bracket prefix')
-                continue
-            stripsubject = subject[closebracketpos+len(endprefix):].replace('\n','')
-            self.subjects.append(stripsubject)
+    maxlength = 80
 
     def test_subject_presence(self):
-        for subject in self.subjects:
-            if not subject or not subject.strip():
+        for subject in Subject.subjects:
+            if not subject.strip():
                 self.fail()
 
     def test_subject_format(self):
-        for subject in self.subjects:
-            if not subject or not subject.strip():
+        for subject in Subject.subjects:
+            if not subject.strip():
                 self.skipTest('Empty subject, no reason to execute subject format test')
             else:
                 try:
@@ -41,7 +26,7 @@ class Subject(Base):
                                ('Pyparsing exception', pe.message)])
 
     def test_subject_length(self):
-        for subject in self.subjects:
+        for subject in Subject.subjects:
             l = len(subject)
             if l > Subject.maxlength:
                 self.fail([('Subject', subject), ('Length', 'Current length %s Max length %s' % (l, Subject.maxlength))])
