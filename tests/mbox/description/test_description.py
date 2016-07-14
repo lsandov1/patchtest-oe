6988c12c9afd6d638a9b0df34f08703b1d5801cb
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-14 06:42:40 -0500

    test_description: clean-up test case
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/mbox/description/test_description.py
+++ tests/mbox/description/test_description.py
@@ -1,16 +1,11 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from base import Base
-from parse_signed_off_by import signed_off_by_mark
 
 class Description(Base):
 
     def test_description_presence(self):
         for description in Description.descriptions:
-            # in case there are no description at all
-            if not description:
-                self.fail()
-            # in case the description are just white spaces
             if not description.strip():
                 self.fail()
 
