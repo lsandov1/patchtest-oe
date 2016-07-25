02dbf3bd9f433c71edf75308b763295482b72b72
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 11:02:51 -0500

    mbox/description: including fix strings
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/mbox/description/test_description.py
+++ tests/mbox/description/test_description.py
@@ -1,9 +1,10 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from base import Base
+from base import Base, fix
 
 class Description(Base):
 
+    @fix("Please include a brief description for your patch")
     def test_description_presence(self):
         for i in xrange(Description.nmessages):
             description = Description.descriptions[i]
