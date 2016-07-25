600f66e47d933b6fd8e114b5be6d3e8f14fa5167
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 12:10:26 -0500

    mbox/subject: include fix strings
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/mbox/subject/test_subject.py
+++ tests/mbox/subject/test_subject.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 
-from base import Base
+from base import Base, fix
 import parse_subject
 from pyparsing import ParseException
 
@@ -9,11 +9,23 @@ class Subject(Base):
 
     maxlength = 80
 
+    @fix("""
+Amend the commit message and include a summary with the following format:
+
+    <target>: <summary>
+
+where <target> is the filename where main code changes apply""")
     def test_subject_presence(self):
         for subject in Subject.subjects:
             if not subject.strip():
                 self.fail()
 
+    @fix("""
+Amend the commit message and include a summary with the following format:
+
+<target>: <summary>
+
+where <target> is the filename where main code changes apply""")
     def test_subject_format(self):
         for subject in Subject.subjects:
             if not subject.strip():
@@ -25,6 +37,7 @@ class Subject(Base):
                     self.fail([('Subject', pe.line),
                                ('Column',  pe.col)])
 
+    @fix("Commit summary must not exceed 80 characters")
     def test_subject_length(self):
         for subject in Subject.subjects:
             l = len(subject)
