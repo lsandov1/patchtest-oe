ef750e990122930e21a5653f0a0bc643ac51f61d
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 13:10:01 -0500

    patch/upstream_status: include fix strings and split test
    There was a single test method doing two checks: presence and format,
    so this change split the method in two. It also addes the fix strings.
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/patch/upstream_status/test_upstream_status.py
+++ tests/patch/upstream_status/test_upstream_status.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from base import Base
+from base import Base, fix
 
 from parse_upstream_status import upstream_status, upstream_status_mark, upstream_status_valid_status
 from pyparsing import ParseException
@@ -21,6 +21,15 @@ class PatchUpstreamStatus(Base):
         # match PatchUpstreamStatus.mark with '+' preceding it
         cls.prog = compile("(?<=\+)%s" % cls.mark)
 
+    @fix("""
+Every patch added next to a recipe must have an Upstream-Status
+specified in the patch header having the value of Pending, Submitted, Accepted, Backport,
+Denied, or Inappropriate. Make sure your are following this format
+
+    Upstream-Status: <status>
+
+NOTE: For more information on the meaning of each status, check
+http://www.openembedded.org/wiki/Commit_Patch_Message_Guidelines""")
     def test_upstream_status_presence(self):
         if not PatchUpstreamStatus.newpatches:
             self.skipTest("There are no new software patches, no reason to test %s presence" % PatchUpstreamStatus.mark)
@@ -29,9 +38,24 @@ class PatchUpstreamStatus(Base):
             payload = str(newpatch)
             if not PatchUpstreamStatus.prog.search(payload):
                 self.fail()
+
+    @fix("""
+Every patch added next to a recipe must have an Upstream-Status
+specified in the patch header having the value of Pending, Submitted, Accepted, Backport,
+Denied, or Inappropriate. Make sure your are following this format
+
+    Upstream-Status: <status>
+
+NOTE: For more information on the meaning of each status, check
+http://www.openembedded.org/wiki/Commit_Patch_Message_Guidelines""")
+    def test_upstream_status_format(self):
+        for newpatch in PatchUpstreamStatus.newpatches:
+            payload = str(newpatch)
+            if not PatchUpstreamStatus.prog.search(payload):
+                continue
             for line in payload.splitlines():
                 if PatchUpstreamStatus.prog.search(payload):
                     try:
                         upstream_status.parseString(line.lstrip('+'))
                     except ParseException as pe:
-                        self.fail([('Parse Exception', str(pe))])
+                        self.fail([('Line', pe.line), ('Column', pe.col)])
