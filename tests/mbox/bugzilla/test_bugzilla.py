1ce6a91b68ea84155d5be3efce9645d0bf83a1d4
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 12:00:03 -0500

    mbox/bugzilla: include fix strings
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/mbox/bugzilla/test_bugzilla.py
+++ tests/mbox/bugzilla/test_bugzilla.py
@@ -2,12 +2,18 @@ import sys, os
 import re
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from base import Base
+from base import Base, fix
 
 class Bugzilla(Base):
     rexp_detect     = re.compile("\[.*YOCTO.*\]", re.IGNORECASE)
     rexp_validation = re.compile("\[YOCTO #(\d+)\]$")
 
+    @fix("""
+Amend the commit message and include the bugzilla entry at the end of the commit description as
+
+    [YOCTO #<bugzilla ID>]
+
+where <bugzilla ID> is the bugzilla entry that this patch fixes""")
     def test_bugzilla_entry_format(self):
         for description in Bugzilla.descriptions:
             for line in description.splitlines():
