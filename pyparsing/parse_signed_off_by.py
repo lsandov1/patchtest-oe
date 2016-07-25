101f8593d4e198c271b3d59a90513feeb390ed51
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 15:11:38 -0500

    patchtest-oe: Include the GPLv2 license to all files plus LICENSE & COPYING files
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- pyparsing/parse_signed_off_by.py
+++ pyparsing/parse_signed_off_by.py
@@ -1,4 +1,22 @@
+#!/usr/bin/python
+
 # signed-off-by pyparsing definition
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 from pyparsing import Literal, OneOrMore, Regex
 from common import word, worddot, at, lessthan, greaterthan, start, end, colon
