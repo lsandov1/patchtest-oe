2e00ea0ee4dc49f239dd9a8436b33e09ee3a7c80
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-07-25 12:43:41 -0500

    patch/cve: include fix strings
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/patch/cve/test_cve.py
+++ tests/patch/cve/test_cve.py
@@ -1,6 +1,6 @@
 import sys, os, re
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from base import Base
+from base import Base, fix
 
 class CVE(Base):
 
@@ -8,6 +8,7 @@ class CVE(Base):
     re_cve_word    = re.compile("\s*\+CVE\s?", re.IGNORECASE)
     re_cve_tag     = re.compile("CVE:(\s+CVE\-\d{4}\-\d+)+", re.IGNORECASE)
 
+    @fix("Please include the CVE, as CVE-xxxx-xxxx, in the subject")
     def test_cve_presence_on_subject(self):
         """
         Checks if CVE-xxxx-xxxx is in subject if CVE word is in the payload.
@@ -17,6 +18,9 @@ class CVE(Base):
                 if not self.re_cve_pattern.search(CVE.subjects[i]):
                     self.fail([('Subject', CVE.subjects[i])])
 
+    @fix("""
+Please include the CVE tag on the patch added to the recipe, see:
+http://openembedded.org/wiki/Commit_Patch_Message_Guidelines#CVE_Patches""")
     def test_cve_tag_format(self):
         """
         Checks if patch contains CVE tag if "CVE-xxxx-xxxx" is in subject.
