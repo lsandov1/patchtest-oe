cf6727df7e1b5ce07bd9a756f83766308c9caab8
Daniela Plascencia <daniela.plascencia@intel.com>
2016-07-26 12:33:20 -0500

    todo/metadata/src_uri: check for src_uri left files
    To test, use the reference patches from selftest/files folder
    
    $ cd <patchtest-oe>/selftest/files
    $ cat SrcUri.test_src_uri_left_files.fail | patchtest - --test-dir <patchtest-oe>/todo/metadata/src_uri/
    $ cat SrcUri.test_src_uri_left_files.pass | patchtest - --test-dir <patchtest-oe>/todo/metadata/src_uri/
    
    Output should match with the patch extension.
    
    NOTE: This check does not cover the following case:
    
    * recipe A remove the patch P from the SRC_URI and the patch itself
    * recipe B still points to P
    
    This should not fail because B still needs P.
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- todo/metadata/src_uri/test_src_uri.py
+++ todo/metadata/src_uri/test_src_uri.py
@@ -1,14 +1,42 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-from base import Base
+from base import Base, fix
+from re import compile
+from subprocess import check_output
 from unittest import skip
 
-@skip('Not implemented yet')
 class SrcUri(Base):
-    def test_src_uri_left_files(self):
-        for patch in SrcUri.patchset:
-            payload = str(patch)
 
+    removepatch_regex = compile('-\s*file://(\S+\.patch)')
+    addpatch_regex    = compile('\+\s*file://(\S+\.patch)')
+    patch_regex       = compile('\.patch$')
+
+    @fix("Amend the patch containing the software patch file removal")
+    def test_src_uri_left_files(self):
+        # get the removed patches indicated on diff data
+        removed_diff_patches = set()
+        for remove_file in SrcUri.patchset.removed_files:
+            path = remove_file.path
+            if self.patch_regex.search(path):
+                removed_diff_patches.add(os.path.basename(path))
+
+        # get the removed patches indicated on the bitbake metadata
+        removed_metadata_patches = set()
+        for payload in SrcUri.payloads:
+            for line in payload.splitlines():
+                removematch = self.removepatch_regex.search(line)
+                addmatch    = self.addpatch_regex.search(line)
+                if removematch:
+                    removed_metadata_patches.add(removematch.group(1))
+                if addmatch:
+                    removed_metadata_patches.remove(addmatch.group(1))
+
+        # sets removed_diff_patches and removed_metadata_patches should be the same
+        # if not, lets check if other recipes need the patch file
+        if removed_metadata_patches.difference(removed_diff_patches):
+            self.fail()
+
+    @skip('pending')
     def test_src_uri_checksums_not_changed(self):
         for patch in SrcUri.patchset:
             payload = str(patch)
