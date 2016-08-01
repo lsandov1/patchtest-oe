e484470546af860f4c6f350a7f8511d601742c77
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-08-01 14:56:17 -0500

    test_patch_upstream_status: search line instead of whole payload
    Also, refactor define the regular expressions in the class definition
    instead of inside the setup class method
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- tests/test_patch_upstream_status.py
+++ tests/test_patch_upstream_status.py
@@ -19,11 +19,15 @@
 
 from base import Base, fix
 
-from parse_upstream_status import upstream_status, upstream_status_mark, upstream_status_valid_status
+from parse_upstream_status import upstream_status, upstream_status_valid_status
+from parse_upstream_status import upstream_status_mark as mark
 from pyparsing import ParseException
 from re import compile, search
 
 class PatchUpstreamStatus(Base):
+
+    upstream_status_mark  = str(mark).strip('"')
+    upstream_status_regex = compile("(?<=\+)%s" % upstream_status_mark)
 
     @classmethod
     def setUpClassLocal(cls):
@@ -33,10 +37,6 @@ class PatchUpstreamStatus(Base):
             if patch.path.endswith('.patch') and patch.is_added_file:
                 cls.newpatches.append(patch)
 
-        cls.mark = str(upstream_status_mark).strip('"')
-
-        # match PatchUpstreamStatus.mark with '+' preceding it
-        cls.prog = compile("(?<=\+)%s" % cls.mark)
 
     @fix("""
 Every patch added next to a recipe must have an Upstream-Status
@@ -49,11 +49,11 @@ NOTE: For more information on the meaning of each status, check
 http://www.openembedded.org/wiki/Commit_Patch_Message_Guidelines""")
     def test_upstream_status_presence(self):
         if not PatchUpstreamStatus.newpatches:
-            self.skipTest("There are no new software patches, no reason to test %s presence" % PatchUpstreamStatus.mark)
+            self.skipTest("There are no new software patches, no reason to test %s presence" % self.upstream_status_mark)
 
         for newpatch in PatchUpstreamStatus.newpatches:
             payload = str(newpatch)
-            if not PatchUpstreamStatus.prog.search(payload):
+            if not self.upstream_status_regex.search(payload):
                 self.fail()
 
     @fix("""
@@ -68,10 +68,10 @@ http://www.openembedded.org/wiki/Commit_Patch_Message_Guidelines""")
     def test_upstream_status_format(self):
         for newpatch in PatchUpstreamStatus.newpatches:
             payload = str(newpatch)
-            if not PatchUpstreamStatus.prog.search(payload):
+            if not self.upstream_status_regex.search(payload):
                 continue
             for line in payload.splitlines():
-                if PatchUpstreamStatus.prog.search(payload):
+                if self.upstream_status_regex.search(line):
                     try:
                         upstream_status.parseString(line.lstrip('+'))
                     except ParseException as pe:
