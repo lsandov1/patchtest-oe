9b96759dadbd387ed1af75ef0d16e00ba153fdcf
Daniela Plascencia <daniela.plascencia@intel.com>
2016-09-06 11:53:33 +0000

    Patchtes-oe: ensures 'signed-off-by' tests accept unicode strings
    Changes pyparsing regex so unicode strings are readable when parsing
    a patch having any. This change prevents the tests to fail and
    giving false positives due to an unreadable format.
    
    [YOCTO #9959]
    
    Signed-off-by: Daniela Plascencia <daniela.plascencia@intel.com>

--- tests/pyparsing/parse_signed_off_by.py
+++ tests/pyparsing/parse_signed_off_by.py
@@ -21,13 +21,12 @@
 from pyparsing import Literal, OneOrMore, Regex
 from common import word, worddot, at, lessthan, greaterthan, start, end, colon
 
-name = OneOrMore(worddot)
-
+name = Regex('\S+.*(?= <)')
 username = OneOrMore(worddot)
 domain = OneOrMore(worddot)
 
 # taken from https://pyparsing-public.wikispaces.com/Helpful+Expressions
-email = Regex(r"(?P<user>[A-Za-z0-9._%+-]+)@(?P<hostname>[A-Za-z0-9.-]+)\.(?P<domain>[A-Za-z]{2,4})")
+email = Regex(r"(?P<user>[A-Za-z0-9._%+-]+)@(?P<hostname>[A-Za-z0-9.-]+)\.(?P<domain>[A-Za-z]{2,})")
 
 email_enclosed = lessthan + email + greaterthan
 
